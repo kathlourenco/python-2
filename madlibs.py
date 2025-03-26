6252e13fa197d68e443f5a432f93424c0787fb9a
kathlourenco <kathclairesl@gmail.com>
2025-03-26 18:41:30 -0300

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+youtuber = "someone"
+
+print("subscribe to" + youtuber) 
+print("subscribe to{}".format(youtuber))
+print(f"subscribe to {youtuber}")
+
+adj = input("adjectitve: ")
+verb1= input("verb: ")
+verb2= input("verb: ")
+famous_person= input("famous person: ")
+
+
+
+
+madlib = f"computer programming is so {adj}. It makes me so excited alll the time because \
+I love to {verb1}. Stay hidrated and {verb2} like you are {famous_person}"
+
+print(madlib)
+
+
